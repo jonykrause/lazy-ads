e68e94735586bbc2dea5e766becc6f65be27be98
jonykrause <buero@jonathan-krause.de>
2014-10-08 16:52:50 +0200

    add commonjs and programmatically usage support

--- src/lazyad-loader.js
+++ src/lazyad-loader.js
@@ -1,4 +1,14 @@
-(function() {
+(function (root, factory) {
+  if (typeof exports === 'object' && typeof require === 'function') {
+    module.exports = factory();
+  } else if (typeof define === "function" && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(factory);
+  } else {
+    factory();
+  }
+}(this, function() {
+
 
     var debug = false;
 
@@ -38,52 +48,52 @@
         }
     }
 
-    // Debounce source: https://github.com/rhysbrettbowen/debounce
-    function debounce(func, wait) {
-        // we need to save these in the closure
-        var timeout, args, context, timestamp;
+    // // Debounce source: https://github.com/rhysbrettbowen/debounce
+    // function debounce(func, wait) {
+    //     // we need to save these in the closure
+    //     var timeout, args, context, timestamp;
 
-        return function() {
+    //     return function() {
 
-            // save details of latest call
-            context = this;
-            args = [].slice.call(arguments, 0);
-            timestamp = new Date();
+    //         // save details of latest call
+    //         context = this;
+    //         args = [].slice.call(arguments, 0);
+    //         timestamp = new Date();
 
-            // this is where the magic happens
-            var later = function() {
+    //         // this is where the magic happens
+    //         var later = function() {
 
-                // how long ago was the last call
-                var last = (new Date()) - timestamp;
+    //             // how long ago was the last call
+    //             var last = (new Date()) - timestamp;
 
-                // if the latest call was less that the wait period ago
-                // then we reset the timeout to wait for the difference
-                if (last < wait) {
-                    timeout = setTimeout(later, wait - last);
+    //             // if the latest call was less that the wait period ago
+    //             // then we reset the timeout to wait for the difference
+    //             if (last < wait) {
+    //                 timeout = setTimeout(later, wait - last);
 
-                    // or if not we can null out the timer and run the latest
-                } else {
-                    timeout = null;
-                    func.apply(context, args);
-                }
-            };
+    //                 // or if not we can null out the timer and run the latest
+    //             } else {
+    //                 timeout = null;
+    //                 func.apply(context, args);
+    //             }
+    //         };
 
-            // we only need to set the timer now if one isn't already running
-            if (!timeout) {
-                timeout = setTimeout(later, wait);
-            }
-        };
-    }
+    //         // we only need to set the timer now if one isn't already running
+    //         if (!timeout) {
+    //             timeout = setTimeout(later, wait);
+    //         }
+    //     };
+    // }
 
-    function addEvent(evnt, elem, func) {
-        if (elem.addEventListener) // W3C DOM
-            elem.addEventListener(evnt, func, false);
-        else if (elem.attachEvent) { // IE DOM
-            elem.attachEvent("on" + evnt, func);
-        } else { // No much to do
-            elem["on" + evnt] = func;
-        }
-    }
+    // function addEvent(evnt, elem, func) {
+    //     if (elem.addEventListener) // W3C DOM
+    //         elem.addEventListener(evnt, func, false);
+    //     else if (elem.attachEvent) { // IE DOM
+    //         elem.attachEvent("on" + evnt, func);
+    //     } else { // No much to do
+    //         elem["on" + evnt] = func;
+    //     }
+    // }
 
 
     // Internals
@@ -287,15 +297,19 @@
         log('Lazy-loaded count: ', counter, timeToComplete);
     };
 
+    // LazyAds.domready = domready;
+
     // dependency on ready.js
-    domready(function() {
+    // domready(function() {
 
-        // watch the windows resize event
-        addEvent('resize', window, debounce(function(e) {
-            LazyAds.init();
-        }, 250));
+    //     // watch the windows resize event
+    //     addEvent('resize', window, debounce(function(e) {
+    //         LazyAds.init();
+    //     }, 250));
 
-        LazyAds.init();
-    });
+    //     LazyAds.init();
+    // });
 
-})();
+    return LazyAds;
+
+}));
