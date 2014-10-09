5e834e35f535c601f15867b8f2e405f71fcc1b91
jonykrause <buero@jonathan-krause.de>
2014-10-09 10:44:48 +0200

    remove obsolete deps

--- dist/lazyad-loader.js
+++ dist/lazyad-loader.js
@@ -1,7 +1,7 @@
 /**
 * lazyad-loader
 * Deliver synchronous ads asynchronously with RWD support without modifying the ad code.
-* Madgex. Build date: 08-10-2014
+* Madgex. Build date: 09-10-2014
 */
 
 // An html parser written in JavaScript
@@ -1802,62 +1802,7 @@ Copyright (c) 2014 Derek Brans, MIT license https://github.com/krux/postscribe/b
             WriteStream: WriteStream
         });
     }());
-}());;/*!
-  * domready (c) Dustin Diaz 2012 - License MIT
-  */
-!function (name, definition) {
-  if (typeof module != 'undefined') module.exports = definition()
-  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
-  else this[name] = definition()
-}('domready', function (ready) {
-
-  var fns = [], fn, f = false
-    , doc = document
-    , testEl = doc.documentElement
-    , hack = testEl.doScroll
-    , domContentLoaded = 'DOMContentLoaded'
-    , addEventListener = 'addEventListener'
-    , onreadystatechange = 'onreadystatechange'
-    , readyState = 'readyState'
-    , loadedRgx = hack ? /^loaded|^c/ : /^loaded|c/
-    , loaded = loadedRgx.test(doc[readyState])
-
-  function flush(f) {
-    loaded = 1
-    while (f = fns.shift()) f()
-  }
-
-  doc[addEventListener] && doc[addEventListener](domContentLoaded, fn = function () {
-    doc.removeEventListener(domContentLoaded, fn, f)
-    flush()
-  }, f)
-
-
-  hack && doc.attachEvent(onreadystatechange, fn = function () {
-    if (/^c/.test(doc[readyState])) {
-      doc.detachEvent(onreadystatechange, fn)
-      flush()
-    }
-  })
-
-  return (ready = hack ?
-    function (fn) {
-      self != top ?
-        loaded ? fn() : fns.push(fn) :
-        function () {
-          try {
-            testEl.doScroll('left')
-          } catch (e) {
-            return setTimeout(function() { ready(fn) }, 50)
-          }
-          fn()
-        }()
-    } :
-    function (fn) {
-      loaded ? fn() : fns.push(fn)
-    })
-})
-;(function (root, factory) {
+}());;(function (root, factory) {
   if (typeof exports === 'object' && typeof require === 'function') {
     module.exports = factory();
   } else if (typeof define === "function" && define.amd) {
